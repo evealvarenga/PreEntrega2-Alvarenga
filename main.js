40c8f44f2222b7cd82a48e80e10fb9eb2f2dfb5d
evealvarenga <ealvarenga095@gmail.com>
2023-03-07 20:02:45 -0300

    07/03
    Modificación de tema.
    Actualización de datos y corroboración de funcionamiento.
    Carrito restante.

--- main.js
+++ main.js
@@ -1,71 +1,103 @@
-//Clases
-class Product {
-    constructor(id, name, precio, sum, stock) {
-        this.id = id;
-        this.name = name;
-        this.precio = precio;
-        this.sum = sum;
-        this.stock = stock;
+//Variables
+let menu = ""
+let submenu = ""
+let menuCarrito = ""
+let cant = 0;
+
+//Listas
+listaEmpanadas = [
+    {ID:1,Tipo: "Carne", Precio: 130, Cantidad: 0},
+    {ID:2,Tipo: "Carne picante", Precio: 110,Cantidad: 0,},
+    {ID:3,Tipo: "Carne suave", Precio: 105,Cantidad: 0},
+    {ID:4,Tipo: "Pollo", Precio: 115,Cantidad: 0},
+    {ID:5,Tipo: "Jamón y Queso", Precio: 120,Cantidad: 0},
+    {ID:6,Tipo: "Caprese", Precio: 130,Cantidad: 0},
+    {ID:7,Tipo: "Jamón, Queso y Huevo", Precio: 130,Cantidad: 0},
+    {ID:8,Tipo: "Humita", Precio: 135,Cantidad: 0},
+]
+
+listaPizzas = [
+    {ID:1,Tipo: "Muzzarela", Precio: 1500,Cantidad: 0},
+    {ID:2,Tipo: "Fugazzeta", Precio: 2100,Cantidad: 0},
+    {ID:3,Tipo: "Jamón y Morrón", Precio: 1550,Cantidad: 0},
+    {ID:4,Tipo: "Peperoni", Precio: 1600,Cantidad: 0},
+    {ID:5,Tipo: "Americana", Precio: 2200,Cantidad: 0},
+]
+
+//Función carrito
+function market(array){
+    let precioTotal = 0;
+    array.forEach((el) => {
+        precioTotal += el.Precio * el.Cantidad
+    })
+        return precioTotal;
     }
 
-    descripcion() {
-        return "ID :" + this.id
-            + "\nNombre :" + this.name
-            + "\nPrecio: $" + this.precio
-    }
-}
 
-class ProductControl  {
-    constructor() {
-        this.listaProductos = []
-    }
-
-    agregarJuego(producto) {
-        this.listaProductos.push(producto)
-    }
-
-    filtrarJuegosPorPrecio(precio) {
-        return this.listaProductos.filter((el) => el.precio <= precio)
-    }
-
-    mostrarJuegos() {
-        this.listaProductos.forEach((el) => {
-            console.log( el.descripcion() )
-        }
-        )
-    }
-}
-
-class Carrito{
-    constructor(listaDeCompra){
-        this.precioTotal = 0
-        this.listaDeCompra = listaDeCompra
-    }
-
-    calcularPrecioTotal(arr){
-        let  precioTotal = 0;
-        arr.forEach( producto => {
-            precioTotal += producto.precio * producto.cantidad
-        })
-        for (const objeto_dentro of this.listaDeCompra) {
-            this.precioTotal = this.precioTotal + objeto_dentro.precio * objeto_dentro.cantidad 
-        }
-        return this.precioTotal
-    }
-}
-
-//Declaración de productos
-const allProducts  = new ProductControl()
-allProducts .agregarJuego(new Product(001, "VOL.1: Balance", 4490, "Otorga balance y humectación a la piel, reduciendo la inflamación y/o enrojecimiento de la misma. Ayuda a controlar el acné y brinda luminosidad.", 50))
-allProducts .agregarJuego(new Product(002, "VOL.2: Restauración", 4990, "Su fórmula combina ácido hialurónico de diversos pesos moleculares con un polímero cruzado para hidratar múltiples niveles de la piel. La vitamina B5 potencia la hidratación y restaura la barrera cutánea dejando una apariencia suave y elástica.", 55))
-allProducts .agregarJuego(new Product(003, "VOL.3: Renovación", 4990, "Su fórmula con Bakuchiol, una alternativa vegana al retinol con propiedades antioxidantes que renueva la textura de la piel suavizando la apariencia de líneas finas y arrugas, evitando la descamación.", 45))
-allProducts .agregarJuego(new Product(004, "Defensa AM/PM", 4400, "Sella la hidratación, protegiendo y fortaleciendo la barrera de la piel. Su fórmula de textura fluida incorpora la tecnología probiótica, combinando la acción del Extracto de Matcha Plus de propiedades antioxidantes, con el Hydraskin, un activo postbiótico que hidrata la piel en profundidad, penetrando hasta 4mm y conservando su efecto por 72hs, para sellarla y protegerla de daños externos.", 50))
-allProducts .agregarJuego(new Product(005, "Energía", 3190, "ENERGÍA es la crema para contorno de ojos que estabas esperando, una fórmula especialmente diseñada en pomo con aplicador metálico.", 50))
-allProducts .agregarJuego(new Product(006, "Máscara Hidratante", 2790, "Esta máscara hidratante es ideal para suavizar, hidratar e iluminar todo tipo de pieles.", 60))
-allProducts .agregarJuego(new Product(006, "Máscara Hidratante", 2790, "Esta máscara hidratante es ideal para suavizar, hidratar e iluminar todo tipo de pieles.", 60))
-allProducts .agregarJuego(new Product(007, "Ampollas de Melatonina", 3590, "Estimula la producción de enzimas antioxidantes para maximizar la regeneración de la piel durante la noche.", 100))
+const carrito = []
 
 //Inicio
+console.log("¡Bienvenido a Sabores Argentinos!")
+menu = Number(prompt("Elija una opción: \n1)Menú de empanadas.\n2)Menú de Pizzas.\n3)Finalizar.",1))
+if(menu != 3){
+    do{
+        if(menu == 1){
+            //Ingreso al menú de empanadas
+            console.log("Nuestro menú de empanadas es el siguiente: ")
+            listaEmpanadas.forEach ( producto => {
+                console.log(producto.ID+") Sabor: "+producto.Tipo+"\nPrecio: $"+producto.Precio)
+            })
+            //Carrito de compras
+            do{
+                menuCarrito = Number(prompt("Ingrese la opción deseada: "))
+                if(listaEmpanadas.some( producto => producto.ID == menuCarrito)){
+                    //////////// 
+                    cant = prompt("Ingrese la cantidad deseada: ")
+                    if (cant > 1 ){
+                        carrito.push((listaEmpanadas.find(producto => producto.ID == menuCarrito).cantidad = cant ))
+                        console.log("¡Nuevo producto agregado al carrito! ")
+                    } else{
+                        console.log("El valor ingresado no es una cantidad válida.")
+                    }
+                    
+                } else {
+                    console.log("No hemos podido validar el valor ingresado.")
+                }
+                submenu = prompt("Si desea seguir comprando empanadas precione cualquier tecla. \nPara volver al menú principal ingrese 'ESC'.").toUpperCase()
+            }while(submenu != "ESC")
+        
 
 
-allProducts .mostrarJuegos()
+        }else if(menu == 2){
+            //Ingreso al menú de pizzas
+            console.log("Nuestro menú de pizzas es el siguiente: ")
+            listaPizzas.forEach ( producto => {
+                console.log(producto.ID+") Sabor: "+producto.Tipo+"\nPrecio: $"+producto.Precio)
+            })
+            //Carrito de compras
+            do{
+                menuCarrito = prompt("Ingrese el sabor deseado: ")
+                if(listaEmpanadas.some(producto => producto.ID == menuCarrito)){
+                    ////////////
+                    cant = prompt("Ingrese la cantidad deseada: ")
+                    if (cant > 1 ){
+                        carrito.push((listaPizzas.find(producto => producto.ID == menuCarrito).cantidad = cant ))
+                        console.log("¡Nuevo producto agregado al carrito!")
+                    } else{
+                        console.log("El valor ingresado no es una cantidad válida.")
+                    }
+                } else {
+                    console.log("No hemos podido validar el valor ingresado.")
+                }
+                submenu = prompt("Si desea seguir comprando pizzas precione cualquier tecla. \nPara volver al menú principal ingrese 'ESC'.").toUpperCase()
+            }while(submenu != "ESC")
+
+        }else{
+            console.log("El valor ingresado no es correcto.")
+        }
+        menu = Number(prompt("Elija una opción: \n1)Menú de empanadas.\n2)Menú de Pizzas.\n3)Finalizar.",1))
+    }while(menu != 3)
+}
+
+
+ console.log("Precio total de su compra: "+ market(carrito))
